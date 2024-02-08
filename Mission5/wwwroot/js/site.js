673b96b6006cbf35fd4c1cf3365326d77b4f88b9
coolwhipki <kirajanegirl@gmail.com>
2024-02-08 11:28:49 -0700

    Add project files.

---
+++ Mission5/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('#btnCalculate').click(function () {
+        var hours = parseFloat($('#hours').val());
+        if (hours === null || isNaN(hours))
+        {
+            hours = 0;
+        }
+        var ratePerHour = parseFloat($('#ratePerHour').val());
+        var total = hours * ratePerHour;
+        if (total < 0) {
+            alert("Please enter a positive number of hours.")
+        }
+        else
+        {
+            $('#total').text( "$ " + total.toFixed(2));
+        }
+    });
+});
